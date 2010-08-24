17a1d71de71aab1756354d07c8586d98ad4239ef
jianyang@taobao.com <jianyang@taobao.com@93222364-424b-453f-a664-057c7d913f5a>
2010-08-24 10:04:31 +0000

    尝试提交哦

--- trunk/src/main/java/com/ql/util/express/DefaultContext.java
+++ trunk/src/main/java/com/ql/util/express/DefaultContext.java
@@ -3,5 +3,5 @@ package com.ql.util.express;
 import java.util.HashMap;
 
 @SuppressWarnings("serial")
-public class DefaultContext<K,V> extends HashMap<K,V> implements IExpressContext<K,V> {
+public class DefaultContext<K,V> extends HashMap<K,V> implements IExpressContext<K,V> { 
 }
