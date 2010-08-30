6b6a3ce712261d93b72681a291f31dc6466ae412
qhlhl2010@gmail.com <qhlhl2010@gmail.com@93222364-424b-453f-a664-057c7d913f5a>
2010-08-30 07:14:53 +0000

    增加 for 循环()的支持 qh = 100;  for(def int i = 1;  i<=10;i = i + 1; ){    qh = qh + 10; } 后续再改进

--- trunk/src/test/java/com/ql/util/express/test/IfTest.java
+++ trunk/src/test/java/com/ql/util/express/test/IfTest.java
@@ -6,7 +6,7 @@ import com.ql.util.express.ExpressRunner;
 public class IfTest extends ExpressRunner {
 	@org.junit.Test
 	public void testDemo() throws Exception{
-		String expressString ="qh = 100; for{def int i = 1;  i<=10;i = i + 1; }{qh = qh + 10;}";
+		String expressString ="qh = 100; for(def int i = 1;  i<=10;i = i + 1;){qh = qh + 10;}";
 		ExpressRunner runner = new ExpressRunner();		
 		DefaultContext<String, Object>  context = new DefaultContext<String, Object>();		
 		context.put("bean", new BeanExample("qhlhl2010@gmail.com"));
