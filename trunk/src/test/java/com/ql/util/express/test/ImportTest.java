651ead2d717146b53274ca45ff45fad49da3aae7
qhlhl2010@gmail.com <qhlhl2010@gmail.com@93222364-424b-453f-a664-057c7d913f5a>
2010-09-17 15:05:49 +0000

    增加import支持，例如： import java.math.*; import com.ql.util.express.test.BeanExample; String abc = new BeanExample("张三").unionName("李四"); return new BigInteger("1000");

---
+++ trunk/src/test/java/com/ql/util/express/test/ImportTest.java
@@ -0,0 +1,25 @@
+package com.ql.util.express.test;
+
+import java.math.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+
+public class ImportTest {
+	@Test
+	public void testImport() throws Exception{	
+		String express ="import java.math.*;" +
+				"import com.ql.util.express.test.BeanExample;" +
+				"abc = new BeanExample(\"张三\").unionName(\"李四\") ;" +
+				"return new BigInteger(\"1000\");";
+		ExpressRunner runner = new ExpressRunner();
+		DefaultContext<String, Object>  context = new DefaultContext<String, Object>();	
+		Object r = runner.execute(express, null, false, context,null,true);
+		Assert.assertTrue("import 实现错误",r.toString().equals("1000"));
+		System.out.println(r);
+		System.out.println(context);		
+	}
+}
