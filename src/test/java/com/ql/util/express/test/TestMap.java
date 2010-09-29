a151102ecf6989fcee2fd66fbbf05f65a2af2ee3
qhlhl2010 <qhlhl2010@gmail.com>
2010-09-29 11:28:25 +0000

    将 exportDef 直接输出到用户传入的上下文中

--- src/test/java/com/ql/util/express/test/TestMap.java
+++ src/test/java/com/ql/util/express/test/TestMap.java
@@ -3,20 +3,20 @@ package com.ql.util.express.test;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.ql.util.express.ExpressRunner;
 import com.ql.util.express.IExpressContext;
 
 public class TestMap {
-	
+
 	@Test
-	public  void testmain() {
+	public void testmain() throws Exception {
 		IExpressContext expressContext = new IExpressContext() {
 			Map map = new HashMap<Object, Object>();
 
 			public Object get(Object key) {
-
 				return map.get(key);
 			}
 
@@ -27,16 +27,12 @@ public class TestMap {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("key1", 1);
 		expressContext.put("map", map);
-		
-		String expressionstr="map.key1";
-		ExpressRunner runner = new ExpressRunner();
-		try {
-			
-			System.out.println(runner.execute(expressionstr, null, null, true, true));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			   
+
+		String expressionstr = "map.key1";
+		ExpressRunner runner = new ExpressRunner(true);
+		Object r =runner.execute(expressionstr, expressContext, null,
+				true, true);
+		Assert.assertTrue("Map∂¡»°¥ÌŒÛ", r.toString().equalsIgnoreCase("1"));
+
 	}
 }
