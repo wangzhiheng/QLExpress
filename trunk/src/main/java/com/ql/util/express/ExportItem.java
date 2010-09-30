a40d3a2486db3de64c88bc4da7a9556b38ffca67
qhlhl2010@gmail.com <qhlhl2010@gmail.com@93222364-424b-453f-a664-057c7d913f5a>
2010-09-30 03:32:31 +0000

    提交ExportItem

---
+++ trunk/src/main/java/com/ql/util/express/ExportItem.java
@@ -0,0 +1,53 @@
+package com.ql.util.express;
+
+
+public class ExportItem{
+	public static String TYPE_ALIAS ="alias";
+	public static String TYPE_DEF ="def";
+	public static String TYPE_FUNCTION ="function";
+	public static String TYPE_MACRO ="macro";
+	String globeName;
+	String name;
+	String type;//def,alias
+	String desc;//类名或者别名
+	public ExportItem(String aName,String aType,String aDesc){
+		this.globeName = aName;
+		this.name = aName;
+		this.type =aType;
+		this.desc = aDesc;	
+	}
+	public ExportItem(String aGlobeName,String aName,String aType,String aDesc){
+		this.globeName = aGlobeName;
+		this.name = aName;
+		this.type =aType;
+		this.desc = aDesc;	
+	}
+	public String toString(){
+		return this.globeName + "["+ this.type +":" + this.name +" " + this.desc + "]";
+	}
+	public String getGlobeName() {
+		return globeName;
+	}
+	public void setGlobeName(String globeName) {
+		this.globeName = globeName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+}
