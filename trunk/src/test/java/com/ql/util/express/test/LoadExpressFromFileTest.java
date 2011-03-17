ca62bfb8642d8f87cf7b86cbff34d327f72b3afd
jianyang@taobao.com <jianyang@taobao.com@93222364-424b-453f-a664-057c7d913f5a>
2011-03-17 07:53:54 +0000

    支持变参

--- trunk/src/test/java/com/ql/util/express/test/LoadExpressFromFileTest.java
+++ trunk/src/test/java/com/ql/util/express/test/LoadExpressFromFileTest.java
@@ -11,22 +11,30 @@ import com.ql.util.express.InstructionSet;
 
 public class LoadExpressFromFileTest {
 	@Test
-	public void testLoadFromFile() throws Exception{	
+	public void testLoadFromFile() throws Exception {
 		ExpressRunner runner = new ExpressRunner(true);
 		ExpressLoader loader = new ExpressLoader(runner);
 		loader.loadExpressFromFile("functiondef");
 		loader.loadExpressFromFile("main");
 		ExportItem[] exports = loader.getExportInfo();
-		for(ExportItem item:exports){
+		for (ExportItem item : exports) {
 			System.out.println(item);
 		}
-		DefaultContext<String, Object>  context = new DefaultContext<String, Object>();		
+		DefaultContext<String, Object> context = new DefaultContext<String, Object>();
 		Log log = new MyLog("玄难测试");
-		Object r = runner.execute(new InstructionSet[]{
-				loader.getInstructionSet("main")
-		}, loader, context, null, null, true,false,log);
-		System.out.println(r);
-		System.out.println(context);
-	}	
-	
+		Object r = runner.execute(new InstructionSet[] { loader
+				.getInstructionSet("main") }, loader, context, null, null,
+				true, false, log);
+		System.out.println("运行结果" + r);
+		System.out.println("context:" + context);
+
+		context = new DefaultContext<String, Object>();
+		r = runner.execute(new InstructionSet[] { runner
+				.parseInstructionSet("initial;累加;累加;return qh;") }, loader,
+				context, null, null, true, false, log);
+
+		System.out.println("运行结果" + r);
+		System.out.println("context:" + context);
+	}
+
 }
