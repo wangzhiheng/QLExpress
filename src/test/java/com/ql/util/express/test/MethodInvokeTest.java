e9e27c559b7dc6e7befcd29f05489c69d2dc3ff1
mutoutoumu <mutoutoumu@93222364-424b-453f-a664-057c7d913f5a>
2012-07-29 01:19:45 +0000

    解决自定义函数返回null的问题,void class 与null的区分

--- src/test/java/com/ql/util/express/test/MethodInvokeTest.java
+++ src/test/java/com/ql/util/express/test/MethodInvokeTest.java
@@ -120,11 +120,12 @@ public class MethodInvokeTest {
 			Object r = runner.execute(express, 
 					expressContext, null, false, false);
 			System.out.println("r=" + r);
-			throw new Exception("return void exception not checked!");
 		}catch(Exception e){
 			e.printStackTrace();
 			System.out.println("return void checked success!");
+			return;
 		}
+		throw new Exception("return void exception not checked!");
 	}
 
 	
